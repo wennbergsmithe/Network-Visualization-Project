b2f4a7e59c1a78628aa6c9e5e7b2e911ed3f2448
Eli Wennberg Smith <32184125+wennbergsmithe@users.noreply.github.com>
2019-06-04 02:09:28 -0400

    Update airportCodes.py

--- airportCodes.py
+++ airportCodes.py
@@ -1,8 +1,8 @@
 
-
 file = open("Airport_Codes.txt", "r")
-
+#dictionary for codes and coords
 dic = {}
+#parsing file
 for line in file:
 	string = ""
 	strs = []
@@ -17,14 +17,14 @@ for line in file:
 	num = leng - 1
 	strs.append(string[:num])
 	string = ""
+	#adding to dictionary
 	dic[strs[0]] = [strs[1], strs[2]]
+#close file
 file.close()
 
-
-
-
-
-file = open("nodes.txt", "r") 
+#open file for nodes
+file = open("nodes.txt", "r")
+#matching codes in nodes file to coordinates collected from other file
 codes = []
 for line in file: 
 	record = False
@@ -40,16 +40,13 @@ for line in file:
 	codes.append(code[1:num])
 
 file.close()
-
+#print results to the console
 num = 0
+
 for code in codes:
-	
 	coords = dic.get(code)
 	if(coords != None):
 		print(str(num) + ","+code+ ","+ coords[0]+ ","+ coords[1])
-
-	
-
 	num+=1
 
 
